6675a23706ad8e5e2e21e3fcb7dbcd319e596631
Enrique Eduardo L.A <108234342+MoorningStarEELA@users.noreply.github.com>
2024-02-22 08:15:39 -0600

    Update ac05.py

--- ac05.py
+++ ac05.py
@@ -10,26 +10,26 @@ def divVncer(lista, page):
         mitad = len(lista) // 2
         # Llamada recursiva para sumar la primera mitad de la lista
         page.add(ft.Text(f"Dividiendo lista en dos mitades: {lista[:mitad]} y {lista[mitad:]}"))
-        suma_izquierda = divVncer(lista[:mitad], page)
+        smnIzq = divVncer(lista[:mitad], page)
         # Llamada recursiva para sumar la segunda mitad de la lista
         suma_derecha = divVncer(lista[mitad:], page)
         # Sumamos los resultados de ambas llamadas recursivas y retornamos la suma
-        page.add(ft.Text(f"Sumando subtotales: {suma_izquierda} y {suma_derecha}"))
-        total = suma_izquierda + suma_derecha
+        page.add(ft.Text(f"Sumando subtotales: {smnIzq} y {suma_derecha}"))
+        total = smnIzq + suma_derecha
         page.add(ft.Text(f"Total parcial: {total}"))
         return total
 
 def main(page):
     def btn_click(e):
         if not txt_cadena.value:
-            txt_cadena.error_text = "Ingrese los numeros separados por espacios"
+            txt_cadena.error_text = "Ingrese los números separados por espacios"
             page.update()
         else:
-            numeros = txt_cadena.value.split()
+            nms = txt_cadena.value.split()
             # Convertimos los elementos de la lista a números enteros
-            numeros = [int(num) for num in numeros]
+            nms = [int(num) for num in nms]
             page.clean()
-            resultado = divVncer(numeros, page)
+            resultado = divVncer(nms, page)
             page.add(ft.Text(f"La suma de los elementos de la lista es: {resultado}"))
 
     txt_cadena = ft.TextField(label="Ingrese los números separados por espacios")
